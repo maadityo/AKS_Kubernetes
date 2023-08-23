9c11142d8c4c252e0fc56b0ac7bc53739e381f33
maadityo <mathias.arya@gmail.com>
2023-08-23 11:07:26 +1000

    update math

--- Module3/buildmt3chainedallacr.ps1
+++ Module3/buildmt3chainedallacr.ps1
@@ -11,7 +11,7 @@ for (($i = 1); $i -lt 6; $i++) {
     az acr build --registry $acrname --image "$($imagename):latest" --file "./Chained/MT3Chained-Step$($step)/Dockerfile" .
 }
 $imagename = "mt3chained-step2-nodejs"
-$folder = "./Chained/MT3Chained-Step2-NodeJS/"
+$folder = ".Module3/MathTrick/Chained/Chained - Shortcut.lnk/Chained/MT3Chained-Step2-NodeJS/"
 Set-Location $folder
 az acr build --registry $acrname --image "$($imagename):latest" --file "Dockerfile" .
 Set-Location ../..
